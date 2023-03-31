035f884e98069b9697903d41e2080c02adba589b
encipher <encipher01@yandex.ru>
2023-03-31 17:50:51 +0300

    Update checksigntx.sh

--- checksigntx.sh
+++ checksigntx.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+clear
 echo -e '\n\e[42mPlease wait... update your system\e[0m\n' 
 sudo apt update -y >> /dev/null 2>&1
-echo -e '\n\e[42m20%\e[0m\n' 
+clear
+echo -e '\n\e[42m..............................\e[0m\n-\033[32m20%\e[0m\n' 
 sudo apt upgrade -y >> /dev/null 2>&1
-echo -e '\n\e[42m40%\e[0m\n' 
+clear
+echo -e '\n\e[42m..............................\e[0m\n-\033[32m40%\e[0m\n' 
 sudo apt install original-awk -y >> /dev/null 2>&1
-echo -e '\n\e[42m60%\e[0m\n' 
+clear
+echo -e '\n\e[42m..............................\e[0m\n-\033[32m60%\e[0m\n' 
 sudo apt install bc -y >> /dev/null 2>&1
-echo -e '\n\e[42m80%\e[0m\n' 
+clear
+echo -e '\n\e[42m..............................\e[0m\n-\033[32m80%\e[0m\n' 
 sudo apt install curl -y >> /dev/null 2>&1
-echo -e '\n\e[42mOK - system updated\e[0m\n' 
+clear
+echo -e '\n\e[42m........ ..OK - system updated\e[0m\n-\033[32m100%\e[0m\n' 
 sleep 2
+
+counter1=0
+counter2=0
 for((;;)); do
 
 # Execute the command and store the output in a variable
@@ -30,14 +39,14 @@ else
 clear
  echo "---------------------------------------------------------------------------------------------------------------------"
 PROPOSER=$(echo $consensus | jq -r '.result.round_state.proposer.address' | sed 's/[{}]//g')
-echo -e "PROPOSER \033[32m$PROPOSER\033[0m"
-echo "---------------------------------------------------------------------------------------------------------------------"
+echo -e  "PROPOSER \033[32m$PROPOSER\033[0m"
+#echo "---------------------------------------------------------------------------------------------------------------------"
 height=$(echo $status | jq -r '.result.sync_info.latest_block_height')
-echo -e "Consensus \033[32m$result\033[0m "
-echo -e "Latest block height: \033[32m$height\033[0m "
+echo -e  "Consensus \033[32m$result\033[0m "
+echo -e  "Latest block height: \033[32m$height\033[0m "
 echo "---------------------------------------------------------------------------------------------------------------------"
 output=$(echo $consensus | jq '.result.round_state.height_vote_set[0].prevotes[]')
-echo "$output" | pr -3 -t -w 120
+echo -e "$output" | pr -3 -t -w 120
 echo "---------------------------------------------------------------------------------------------------------------------"
 
 address1=$(echo $consensus | jq -r '.result.round_state.proposer.address')
@@ -55,7 +64,7 @@ output2=${output1%% *}  # Remove everything after the first space
 # echo "--------------------------------------------------------------------"
 #echo $output | pr -1 -t
  #echo "--------------------------------------------------------------------"
-echo -e $input
+echo -e  $input
 echo "---------------------------------------------------------------------------------------------------------------------"
 #echo -e $output1
  #echo "--------------------------------------------------------------------"
@@ -63,18 +72,24 @@ echo "--------------------------------------------------------------------------
 # echo "--------------------------------------------------------------------"
 
 if [ "$address1" != "$address2" ]; then
-  echo -e "\033[31m YOU ARE NOT PROPOSER \033[0m"
+  echo -e  "\033[31m YOU ARE NOT PROPOSER \033[0m"
   echo "---------------------------------------------------------------------------------------------------------------------"
   if [ "$address3" == "$output2" ]; then
-  echo -e "\033[32m YOU ARE A SIGNER \033[0m"
+  echo -e  "\033[32m YOU ARE A SIGNER \033[0m"
+  counter1=$(($counter1+1))
+
   else
-   echo -e "\033[31m YOU ARE NOT A SIGNER \033[0m"
+   echo -e  "\033[31m YOU ARE NOT A SIGNER \033[0m"
   fi
 else
-  echo -e "\033[32m YOU ARE PROPOSER \033[0m"
+  echo -e  "\033[32m YOU ARE PROPOSER \033[0m"
+    counter2=$(($counter2+1))
 fi
  echo "---------------------------------------------------------------------------------------------------------------------"
-
+echo -e " Node signed   the block \033[32m $counter1 \033[0m  times "
+echo -e " Node proposed the block \033[32m $counter2 \033[0m  times"
+echo -e "\033[32m PRESS -- CTRL+C -- TO EXIT \033[0m "
 fi
 sleep 0.2
+
 done
